6c959fd19ea7b15d576942ade7fdf89df506b575
hongthietpkh <hongthiet1994@gmail.com>
2019-10-30 22:25:40 +0700

    QMessageBox 2

--- learning_QT/mainwindow.cpp
+++ learning_QT/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QMessageBox.h"
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
@@ -16,10 +17,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-  //QMessageBox::about(this,"My Title","This is my custom message");
-  //QMessageBox::aboutQt(this,"My Title");
-  //QMessageBox::critical(this,"My Title","This is my custom message");
+    //QMessageBox::about(this,"My Title","This is my custom message");
+    //QMessageBox::aboutQt(this,"My Title");
+    //QMessageBox::critical(this,"My Title","This is my custom message");
     //QMessageBox::information(this,"My Title","This is my custom message");
     //QMessageBox::question(this,"My Title","This is my custom message");
-    QMessageBox::warning(this,"My Title","This is my custom message");
+    //QMessageBox::warning(this,"My Title","This is my custom message");
+    QMessageBox::StandardButton reply = QMessageBox::question(this,"My Title","This is my custom message",QMessageBox::Yes | QMessageBox::No);
+    if(reply == QMessageBox::Yes)
+    {
+        QApplication::quit();
+    }
+    else
+    {
+        qDebug() << "No is clicked";
+    }
 }
