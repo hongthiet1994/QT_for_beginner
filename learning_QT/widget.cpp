b172f3944d2afa4b2880e925615bfd20e9868f1e
hongthietpkh <hongthiet1994@gmail.com>
2019-10-30 16:02:18 +0700

    click button to show text

--- learning_QT/widget.cpp
+++ learning_QT/widget.cpp
@@ -1,6 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
-
+uint32_t ui32_counter_button_Helloworld = 0;
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -13,3 +13,16 @@ Widget::~Widget()
     delete ui;
 }
 
+
+void Widget::on_bt_Helloworld_clicked()
+{
+    ui32_counter_button_Helloworld++;
+    if(ui32_counter_button_Helloworld%2==0)
+    {
+       ui->label->setText("Hello world ");
+    }
+    else
+    {
+        ui->label->clear();
+    }
+}
--- learning_QT/widget.h
+++ learning_QT/widget.h
@@ -15,6 +15,9 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_bt_Helloworld_clicked();
+
 private:
     Ui::Widget *ui;
 };
